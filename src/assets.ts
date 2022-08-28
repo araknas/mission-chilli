bf8434f6c0c7d4cff6b68cada8a22270d410b590
araknas <giedriusebay@gmail.com>
2022-08-28 12:57:42 +0300

    feat: generate random red balls

--- src/assets.ts
+++ src/assets.ts
@@ -3,4 +3,5 @@ export const assets = [
   { name: 'Chilli in grill', url: './real-chilli/chilli-in-grill.png' },
   { name: 'Simple cat', url: './cartoon-chilli/simple-cat.png' },
   { name: 'Milky way', url: './wallpappers/milky-way.jpg' },
+  { name: 'Red ball', url: './toys/red-ball.png' },
 ];
--- src/scenes/PlayRoomScene.ts
+++ src/scenes/PlayRoomScene.ts
@@ -1,10 +1,11 @@
-import { Container, Sprite } from 'pixi.js';
+import { Container, DisplayObject, Sprite } from 'pixi.js';
 import { Keyboard } from '../Keyboard';
 import { IScene, Manager } from '../Manager';
 
 const HERO_STEP_SIZE = 3;
 export class PlayRoomScene extends Container implements IScene {
   private hero: Sprite;
+  private toys: Sprite[] = [];
   constructor() {
     super();
     this.hero = Sprite.from('Simple cat');
@@ -15,7 +16,49 @@ export class PlayRoomScene extends Container implements IScene {
     // Scale dynamically later (1/10 background size?)
     this.hero.scale = { x: 0.2, y: 0.2 };
     this.addChild(this.hero);
+    this.generateToys();
   }
+
+  private generateToys(): void {
+    const toy1 = this.generateRedBall();
+    const toy2 = this.generateRedBall();
+    const toy3 = this.generateRedBall();
+    const toy4 = this.generateRedBall();
+
+    this.dropInRandomLocation(toy1);
+    this.dropInRandomLocation(toy2);
+    this.dropInRandomLocation(toy3);
+    this.dropInRandomLocation(toy4);
+
+    this.addChild(toy1);
+    this.addChild(toy2);
+    this.addChild(toy3);
+    this.addChild(toy4);
+  }
+
+  private dropInRandomLocation(object: DisplayObject): void {
+    const minX = Manager.width / 2 + 20;
+    const maxX = Manager.width - 20;
+    const minY = Manager.height + 20;
+    const maxY = -20;
+
+    const randomX = this.randomIntFromInterval(minX, maxX);
+    const randomY = this.randomIntFromInterval(minY, maxY);
+    object.x = randomX;
+    object.y = randomY;
+  }
+
+  private randomIntFromInterval(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  private generateRedBall(): DisplayObject {
+    const toy = Sprite.from('Red ball');
+    // Scale dynamically later (1/30 background size?)
+    toy.scale = { x: 0.05, y: 0.05 };
+    return toy;
+  }
+
   public update(framesPassed: number): void {
     if (Keyboard.arrowUp() && this.hero.y > 0) {
       console.debug('Hero should go up');
